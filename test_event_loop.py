67ef92b91c6f5bf22f91461a7a365165b13e7c2c
Michael Adkins <michael@prefect.io>
2022-01-13 17:25:27 -0600

    Use `is` instead of `==`

--- test_event_loop.py
+++ test_event_loop.py
@@ -5,15 +5,15 @@ LOOP = None
 async def test_with_event_loop():
     global LOOP
     LOOP = asyncio.get_running_loop()
-    assert asyncio.get_running_loop() == LOOP
+    assert asyncio.get_running_loop() is LOOP
 
 
 def test_use_asyncio_run():
     async def check_loop():
-        assert LOOP != asyncio.get_running_loop()
+        assert LOOP is not asyncio.get_running_loop()
     asyncio.run(check_loop())
 
 
 async def test_event_loop_after_sync_run():
-    assert LOOP == asyncio.get_running_loop()
+    assert LOOP is asyncio.get_running_loop()
 
